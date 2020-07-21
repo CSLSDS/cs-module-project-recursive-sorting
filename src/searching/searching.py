d66773de2791b8d08b742573fa60a4343455b3e4
cslsds <lambdadatasciencelearning@gmail.com>
2020-07-21 16:37:10 -0400

    mod2 complete; 1/2 stretch incomplete

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,17 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+    # check if feasible
+    if len(arr) == 0:
+        return -1
+    
+    mid = (start + end) // 2
+
+    if arr[mid] == target:
+        return mid
+    elif target < arr[mid]:
+        return binary_search(arr, target, start, mid)
+    else:
+        return binary_search(arr, target, mid, end)
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -10,5 +21,31 @@ def binary_search(arr, target, start, end):
 # You can implement this function either recursively 
 # or iteratively
 def agnostic_binary_search(arr, target):
-    # Your code here
+    # check if feasible
+    if len(arr) == 0:
+        return -1
+    lower = 0
+    upper = len(arr)-1
 
+    # define ascending condition
+    ascending = arr[lower] < arr[upper]
+    # while something remains to be sorted/iterated over
+    while lower <= upper:
+        mid = (lower + upper) // 2
+
+        if arr[mid] == target:
+            return mid
+        if ascending:
+
+            if target < arr[mid]:
+                upper = mid - 1
+            else:
+                lower = mid + 1
+        else:
+
+            if target > arr[mid]:
+                upper = mid - 1
+            else:
+                lower = mid + 1
+    
+    return -1 
